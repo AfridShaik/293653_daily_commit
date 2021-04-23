1649475618599624fd4732d733a24c8864e8fba4
AfridShaik <afridtech1@gmail.com>
2021-04-23 19:45:47 +0530

    if else program 2 done

---
+++ Submission/ifelse_2.py
@@ -0,0 +1,11 @@
+string =input()
+if(string=="lol"):
+    print("laughing out loud")
+elif(string=="rofl"):
+    print("rolling on the floor laughing")
+elif(string=="lmk"):
+    print("let me know")
+elif(string=="smh"):
+    print("shaking my head")
+else:
+    print("you didn't entered anything!")
