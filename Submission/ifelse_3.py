44feaad5c4add46af8fcb14431505b2f8430a042
AfridShaik <afridtech1@gmail.com>
2021-04-23 19:50:37 +0530

    if else program  done

---
+++ Submission/ifelse_3.py
@@ -0,0 +1,5 @@
+num=int(input())
+if n%2==0:
+    print("price =" ,float(num*120.00))
+else:
+    print("price =",float(num*123.00))
---
+++ Submission/ifelse_4.py
@@ -0,0 +1,9 @@
+data_type = input("Enter the data type")
+if type(data_type) == int:
+    print("integer")
+elif type(data_type) == str:
+    print("string")
+elif type(data_type) == float:
+    print("float")
+elif type(data_type) == complex:
+    print("complex")
